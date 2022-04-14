1dec920e130519f29702c8150ecb785693a6f716
leo <leo88728@gmail.com>
2022-04-14 19:21:15 +0800

    initt

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     // 选项...
     lintOnSave:false,
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://github.com/lleooo/MovieApp.git/'
-    : '/'
+    publicPath: './'
+    // process.env.NODE_ENV === 'production'
+    // ? '/https://github.com/lleooo/MovieApp.git/'
+    // : '/'
   }
